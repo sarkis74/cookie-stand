7826c307dfcbea8ab8265940fa31d6c7f00549c2
Sarkis Aghazarian <sarkis_aghazarian@yahoo.com>
2018-09-23 17:11:04 -0700

    updated

---
+++ main.js
@@ -0,0 +1,49 @@
+var firstPike = {
+    minCust: 23,
+    maxCust: 65,
+    avgSale: 6.3,
+
+    custPerHr: function(num) {
+        return Math.floor(Math.random() * Math.floor()) * this.avgSale
+    }
+}
+
+var seaTac = {
+    minCust: 3,
+    maxCust: 24,
+    avgSale: 1.2,
+
+    custPerHr: function(num) {
+        return Math.floor(Math.random() * Math.floor()) * this.avgSale
+    }
+}
+
+var seaCent = {
+    minCust: 11,
+    maxCust: 38,
+    avgSale: 3.7,
+
+    custPerHr: function(num) {
+        return Math.floor(Math.random() * Math.floor()) * this.avgSale
+    }
+}
+
+var capHill = {
+    minCust: 20,
+    maxCust: 38,
+    avgSale: 2.3,
+
+    custPerHr: function(num) {
+        return Math.floor(Math.random() * Math.floor()) * this.avgSale
+    }
+}
+
+var alki = {
+    minCust: 2,
+    maxCust: 16,
+    avgSale: 4.6,
+
+    custPerHr: function(num) {
+        return Math.floor(Math.random() * Math.floor()) * this.avgSale
+    }
+}
