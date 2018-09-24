f2fd945236df50d1df2c61d97e88bbaf72853f98
sarkis74 <42852642+sarkis74@users.noreply.github.com>
2018-09-23 20:38:43 -0700

    Update main.js

--- main.js
+++ main.js
@@ -3,67 +3,133 @@ var firstPike = {
     maxCust: 65,
     avgSale: 6.3,
 
-    custPerHr: function() {
-        num = Math.floor(Math.random() * Math.floor()) 
-    },
+    cookPerHr: function() {
+        var list = document.createElement('ul');//Create list element
 
-    cookPurch: function() {
-        cook = num * this.avgSale
-    }
+        for(var i = 0; i < 15; i++) {
+            // Create the list item:
+            var item = document.createElement('li');
+
+            num1 = Math.floor(Math.random() * 100) * Math.ceil(this.avgSale);
+            return num1;
+            myArr1 = [];
+            myArr1.push(num1)
+
+            // Placing contents:
+            item.appendChild(document.createTextNode(myArr1[i]));
+
+            // Add it to list:
+            list.appendChild(item);
+        }
+    },
 }
+
 
 var seaTac = {
     minCust: 3,
     maxCust: 24,
     avgSale: 1.2,
 
-    custPerHr: function() {
-        num = Math.floor(Math.random() * Math.floor()) 
-    },
+    cookPerHr: function() {
+        var list = document.createElement('ul');//Create list element
 
-    cookPurch: function() {
-        cook = num * this.avgSale
-    }
+        for(var i = 0; i < 15; i++) {
+            // Create the list item:
+            var item = document.createElement('li');
+
+            num2 = Math.floor(Math.random() * 100) * Math.ceil(this.avgSale);
+            return num2;
+            myArr2 = [];
+            myArr2.push(num2)
+
+            // Placing contents:
+            item.appendChild(document.createTextNode(myArr2[i]));
+
+            // Add it to list:
+            list.appendChild(item);
+        }
+    },
 }
+
+    
+
 
 var seaCent = {
     minCust: 11,
     maxCust: 38,
     avgSale: 3.7,
 
-    custPerHr: function() {
-        num = Math.floor(Math.random() * Math.floor()) 
-    },
+    cookPerHr: function() {
+        var list = document.createElement('ul');//Create list element
 
-    cookPurch: function() {
-        cook = num * this.avgSale
-    }
+        for(var i = 0; i < 15; i++) {
+            // Create the list item:
+            var item = document.createElement('li');
+
+            num3 = Math.floor(Math.random() * 100) * Math.ceil(this.avgSale);
+            return num3;
+            myArr3 = [];
+            myArr3.push(num3)
+
+            // Placing contents:
+            item.appendChild(document.createTextNode(myArr3[i]));
+
+            // Add it to list:
+            list.appendChild(item);
+        }
+    },
 }
+
 
 var capHill = {
     minCust: 20,
     maxCust: 38,
     avgSale: 2.3,
 
-    custPerHr: function(num) {
-        return Math.floor(Math.random() * Math.floor()) * this.avgSale
-    },
+    cookPerHr: function() {
+        var list = document.createElement('ul');//Create list element
 
-    cookPurch: function() {
-        cook = num * this.avgSale
-    }
+        for(var i = 0; i < 15; i++) {
+            // Create the list item:
+            var item = document.createElement('li');
+
+            num4 = Math.floor(Math.random() * 100) * Math.ceil(this.avgSale);
+            return num4;
+            myArr4 = [];
+            myArr4.push(num4)
+
+            // Placing contents:
+            item.appendChild(document.createTextNode(myArr4[i]));
+
+            // Add it to list:
+            list.appendChild(item);
+        }
+    },
 }
+
 
 var alki = {
     minCust: 2,
     maxCust: 16,
     avgSale: 4.6,
 
-    custPerHr: function(num) {
-        return Math.floor(Math.random() * Math.floor()) * this.avgSale
-    },
+    cookPerHr: function() {
+        var list = document.createElement('ul');//Create list element
 
-    cookPurch: function() {
-        cook = num * this.avgSale
-    }
+        for(var i = 0; i < 15; i++) {
+            // Create the list item:
+            var item = document.createElement('li');
+
+            num5 = Math.floor(Math.random() * 100) * Math.ceil(this.avgSale);
+            return num5;
+            myArr5 = [];
+            myArr5.push(num5)
+
+            //Placing contents:
+            item.appendChild(document.createTextNode(myArr1[i]));
+
+            // Add to the list:
+            list.appendChild(item);
+        }
+    },
 }
