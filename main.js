5742d55c5ba0d18c423f7fefb440136012fc2a6a
Sarkis Aghazarian <sarkis_aghazarian@yahoo.com>
2018-09-24 20:50:34 -0700

    latest commit

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
 var firstPike = {
+    name: "First and Pike",
     minCust: 23,
     maxCust: 65,
     avgSale: 6.3,
 
     cookPerHr: function() {
         //Create list element
-        var list = document.createElement('ul');
+        // var header = document.createElement('h6');   // Create a <h2> element
+        // header.textContent = this.name
+
+        var listContainer = document.createElement('div');
+        document.getElementsByTagName('body')[0].appendChild(listContainer);
+
+        // var header = document.createElement('h6');   // Create a <h2> element
+        // header.textContent = this.name;
+        // document.getElementsByTagName('h6')[0].appendChild(header);                                                   
+        // var text = document.createTextNode("1st and Pike");     // Create a text node
+        // header.appendChild(text);   // Append the text to <h6> 
+
+        var listEl = document.createElement('ul');
+        document.getElementsByTagName('ul')[0].appendChild(listEl);
+
+        listContainer.appendChild(listEl);
 
         var myArr1 = [];
 
@@ -14,19 +30,26 @@ var firstPike = {
             var item = document.createElement('li');
 
             // Creates random number of cookies sold each hour and stores all 15 results in array
-            num1 = Math.floor(Math.random() * 10) * Math.ceil(this.avgSale);
+            num1 = Math.floor(Math.random() * (this.max - this.min) + this.min);
             
             myArr1.push(num1);
             console.log(myArr1);
 
             // Add the item text
-            list.innerHTML = myArr1[i];
+            var meridiem = 'am';
+            if(i > 4) {
+                meridiem = 'pm';
+            }
+            var mod = (18 + i) % 12; 
+            var line = mod + meridiem + ' : ' + myArr1[i] + ' cookies';
+            // list.innerHTML = myArr1[i];
 
             // Placing contents:
-            //item.appendChild(document.createTextNode((18 + i - 12) + ' am: ' + myArr1[i] + ' cookies'));
+            item.innerHTML = line;
 
-            // Add it to list:
-            list.appendChild(item);
+
+            //var newText = document.createTextNode(line);
+            listEl.appendChild(item);
 
         } return myArr1;
     },
@@ -42,7 +65,18 @@ var seaTac = {
 
     cookPerHr: function() {
         //Create list element
-        var list = document.createElement('ul');
+        var listContainer = document.createElement('div');
+        document.getElementsByTagName('body')[0].appendChild(listContainer);
+
+        // var header = document.createElement('h6');   // Create a <h2> element
+        // document.getElementsByTagName('h6')[0].appendChild(header);                                                   
+        // var text = document.createTextNode("SeaTac Airport");     // Create a text node
+        // header.appendChild(text);   // Append the text to <h2> 
+
+        var listEl = document.createElement('ul');
+        document.getElementsByTagName('ul')[0].appendChild(listEl);
+
+        listContainer.appendChild(listEl);
 
         var myArr2 = [];
 
@@ -57,20 +91,26 @@ var seaTac = {
             console.log(myArr2);
 
             // Add the item text
-            list.innerHTML = myArr2[i];
+            var meridiem = 'am';
+            if(i > 4) {
+                meridiem = 'pm';
+            }
+            var mod = (18 + i) % 12; 
+            var line = mod + meridiem + ' : ' + myArr2[i] + ' cookies';
+            // list.innerHTML = myArr1[i];
 
             // Placing contents:
-            //item.appendChild(document.createTextNode((18 + i - 12) + ' am: ' + myArr2[i] + ' cookies'));
+            item.innerHTML = line;
 
-            // Add it to list:
-            list.appendChild(item);
+
+            //var newText = document.createTextNode(line);
+            listEl.appendChild(item);
 
         } return myArr2;
     },
 }
 
-    
-
+seaTac.cookPerHr();
 
 var seaCent = {
     minCust: 11,
@@ -79,7 +119,18 @@ var seaCent = {
 
     cookPerHr: function() {
         //Create list element
-        var list = document.createElement('ul');
+        var listContainer = document.createElement('div');
+        document.getElementsByTagName('body')[0].appendChild(listContainer);
+
+        // var header = document.createElement('h6');   // Create a <h2> element
+        // document.getElementsByTagName('h6')[0].appendChild(header);                                                   
+        // var text = document.createTextNode("Seattle Center");     // Create a text node
+        // header.appendChild(text);   // Append the text to <h2> 
+
+        var listEl = document.createElement('ul');
+        document.getElementsByTagName('ul')[0].appendChild(listEl);
+
+        listContainer.appendChild(listEl);
 
         var myArr3 = [];
 
@@ -94,17 +145,26 @@ var seaCent = {
             console.log(myArr3);
 
             // Add the item text
-            list.innerHTML = myArr3[i];
+            var meridiem = 'am';
+            if(i > 4) {
+                meridiem = 'pm';
+            }
+            var mod = (18 + i) % 12; 
+            var line = mod + meridiem + ' : ' + myArr3[i] + ' cookies';
+            // list.innerHTML = myArr1[i];
 
             // Placing contents:
-            //item.appendChild(document.createTextNode((18 + i - 12) + ' am: ' + myArr3[i] + ' cookies'));
+            item.innerHTML = line;
 
-            // Add it to list:
-            list.appendChild(item);
+
+            //var newText = document.createTextNode(line);
+            listEl.appendChild(item);
 
         } return myArr3;
     },
 }
+
+seaCent.cookPerHr();
 
 
 var capHill = {
@@ -114,33 +174,52 @@ var capHill = {
 
     cookPerHr: function() {
         //Create list element
-        var list = document.createElement('ul');
+        var listContainer = document.createElement('div');
+        document.getElementsByTagName('body')[0].appendChild(listContainer);
+
+        // var header = document.createElement('h6');   // Create a <h2> element
+        // document.getElementsByTagName('h6')[0].appendChild(header);                                                   
+        // var text = document.createTextNode("Capitol Hill");     // Create a text node
+        // header.appendChild(text);   // Append the text to <h2>
+
+        var listEl = document.createElement('ul');
+        document.getElementsByTagName('ul')[0].appendChild(listEl);
+
+        listContainer.appendChild(listEl);
 
         var myArr4 = [];
 
         for(var i = 0; i < 15; i++) {
-            // Creates the list item:
-            var item = document.createElement('li');
+           // Creates the list item:
+           var item = document.createElement('li');
 
-            // Creates random number of cookies sold each hour and stores all 15 results in array
-            num1 = Math.floor(Math.random() * 10) * Math.ceil(this.avgSale);
-            
-            myArr4.push(num4);
-            console.log(myArr4);
+           // Creates random number of cookies sold each hour and stores all 15 results in array
+           num4 = Math.floor(Math.random() * 10) * Math.ceil(this.avgSale);
+           
+           myArr4.push(num4);
+           console.log(myArr4);
 
-            // Add the item text
-            list.innerHTML = myArr4[i];
+           // Add the item text
+           var meridiem = 'am';
+           if(i > 4) {
+               meridiem = 'pm';
+           }
+           var mod = (18 + i) % 12; 
+           var line = mod + meridiem + ' : ' + myArr4[i] + ' cookies';
+           // list.innerHTML = myArr4[i];
 
-            // Placing contents:
-            //item.appendChild(document.createTextNode((18 + i - 12) + ' am: ' + myArr4[i] + ' cookies'));
+           // Placing contents:
+           item.innerHTML = line;
 
-            // Add it to list:
-            list.appendChild(item);
+
+           //var newText = document.createTextNode(line);
+           listEl.appendChild(item);
 
         } return myArr4;
     },
 }
 
+capHill.cookPerHr();
 
 var alki = {
     minCust: 2,
@@ -148,8 +227,19 @@ var alki = {
     avgSale: 4.6,
 
     cookPerHr: function() {
-        //Create list element
-        var list = document.createElement('ul');
+           //Create list element
+        var listContainer = document.createElement('div');
+        document.getElementsByTagName('body')[0].appendChild(listContainer);
+
+        // var header = document.createElement('h6');   // Create a <h2> element
+        // header.appendChild(document.createTextNode('Alki'));
+        // var text = document.createTextNode("Alki");     // Create a text node
+        // header.appendChild(text);   // Append the text to <h2>
+
+        var listEl = document.createElement('ul');
+        document.getElementsByTagName('ul')[0].appendChild(listEl);
+
+        listContainer.appendChild(listEl);
 
         var myArr5 = [];
 
@@ -160,18 +250,28 @@ var alki = {
             // Creates random number of cookies sold each hour and stores all 15 results in array
             num5 = Math.floor(Math.random() * 10) * Math.ceil(this.avgSale);
             
-            myArr5.push(num1);
-            console.log(myArr1);
+            myArr5.push(num5);
+            console.log(myArr5);
 
             // Add the item text
-            list.innerHTML = myArr5[i];
+            var meridiem = 'am';
+            if(i > 4) {
+                meridiem = 'pm';
+            }
+            var mod = (18 + i) % 12; 
+            var line = mod + meridiem + ' : ' + myArr5[i] + ' cookies';
+            // list.innerHTML = myArr1[i];
 
             // Placing contents:
-            //item.appendChild(document.createTextNode((18 + i - 12) + ' am: ' + myArr5[i] + ' cookies'));
+            item.innerHTML = line;
 
-            // Add it to list:
-            list.appendChild(item);
+
+            //var newText = document.createTextNode(line);
+            listEl.appendChild(item);
 
         } return myArr5;
     },
 }
+
+alki.cookPerHr();
+
